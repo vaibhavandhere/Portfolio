238da4f6b68042d8f552caa1abc21d7b990651a4
Andhere <vandhere@amazon.com>
2026-08-17 18:30:40 -0500

    fix: ensure avatar is always strictly centered across all screen resolutions

--- src/components/utils/GsapScroll.ts
+++ src/components/utils/GsapScroll.ts
@@ -69,7 +69,12 @@ export function setCharTimeline(
       tl1
         .fromTo(character.rotation, { y: 0 }, { y: 0.7, duration: 1 }, 0)
         .to(camera.position, { z: 22 }, 0)
-        .fromTo(".character-model", { x: 0 }, { x: "-25%", duration: 1 }, 0)
+        .fromTo(
+          ".character-model",
+          { xPercent: -50, x: 0 },
+          { xPercent: -50, x: "-25%", duration: 1 },
+          0
+        )
         .to(".landing-container", { opacity: 0, duration: 0.4 }, 0)
         .to(".landing-container", { y: "40%", duration: 0.8 }, 0)
         .fromTo(".about-me", { y: "-50%" }, { y: "0%" }, 0);
@@ -88,8 +93,8 @@ export function setCharTimeline(
         .to(".about-section", { opacity: 0, delay: 3, duration: 2 }, 0)
         .fromTo(
           ".character-model",
-          { pointerEvents: "inherit" },
-          { pointerEvents: "none", x: "-12%", delay: 2, duration: 5 },
+          { pointerEvents: "inherit", xPercent: -50, x: "-25%" },
+          { pointerEvents: "none", xPercent: -50, x: "-12%", delay: 2, duration: 5 },
           0
         )
         .to(character.rotation, { y: 0.92, x: 0.12, delay: 3, duration: 3 }, 0)
@@ -161,8 +166,8 @@ export function setCharTimeline(
       tl3
         .fromTo(
           ".character-model",
-          { y: "0%" },
-          { y: "-100%", duration: 4, ease: "none", delay: 1 },
+          { y: "0%", xPercent: -50, x: "-12%" },
+          { y: "-100%", xPercent: -50, x: "-12%", duration: 4, ease: "none", delay: 1 },
           0
         )
         .fromTo(".whatIDO", { y: 0 }, { y: "15%", duration: 2 }, 0)
