1c0de350d7957c4471758807ebeb6149682c178e
Andhere <vandhere@amazon.com>
2026-08-17 17:11:32 -0500

    fix: resolve gsap-trial dependencies and type definitions

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module "gsap-trial";
+declare module "gsap-trial/all";
+declare module "gsap-trial/ScrollTrigger";
+declare module "gsap-trial/ScrollSmoother";
+declare module "gsap-trial/SplitText";
+declare module "gsap-trial/InertiaPlugin";
+declare module "gsap-trial/MorphSVGPlugin";
+declare module "gsap-trial/DrawSVGPlugin";
+declare module "gsap-trial/ScrambleTextPlugin";
